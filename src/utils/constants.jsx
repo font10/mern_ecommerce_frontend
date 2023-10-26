d76b69062ad099706ede40f45bd6c5808976e2ac
font10 <dfont93@gmail.com>
2023-10-26 15:58:13 +0200

    create modify  done

---
+++ src/utils/constants.jsx
@@ -0,0 +1,12 @@
+export const categories = ['Lifestyle', 'Jordan', 'Running', 'Baloncesto', 'Fútbol', 'Gym y training', 'Skateboard', 'Golf', 'Tenis']
+export const size = ['36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48']
+export const rangePrice = ['0-50', '51-100', '101-150', '151-200', '201-250', '251-300']
+export const rangeStars = ['1', '2', '3', '4', '5'] 
+export const gender = ['Male', 'Female', 'Child']
+
+export const filterMenus = [
+  { name: 'Category', range: categories },
+  { name: 'Price', range: rangePrice },
+  { name: 'Size', range: size },
+  { name: 'Stars', range: rangeStars },
+]
